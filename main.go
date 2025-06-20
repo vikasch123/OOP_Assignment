a2047785bc21ce73ca8cd51d399aece1bab97d6b
vikasch123 <kvikaskumar004@gmail.com>
2025-06-20 12:19:27 +0530

    add the driver code in main.go

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
-
-//import "fmt"
-
-func main(){
-	stud:=CreateNewStudent("Vikas","RA2211031010132")
-	stud.Name()
-	stud.Regno()
-	stud.Display()
+import "fmt"
+func main() {
+// ---------------- L1 -----------------
+st := CreateNewStudent("1", "Alice Johnson")
+c := NewCourse(101, "Intro to Go")
+st.Display()
+// ---------------- L2 -----------------
+en1 := NewEnrollment(st, c, &PercentageGrader{}, 0.83)
+en2 := NewEnrollment(st, c, &PassFailGrader{passMark: 0.5}, 0.83)
+fmt.Println(en1)
+fmt.Println(en2) // demonstrates polymorphism
 }
